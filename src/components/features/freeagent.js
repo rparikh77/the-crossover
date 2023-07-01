ca069742d9680a37d3a6438dc2d495b784a95528
Rohan Parikh <raparikh10@gmail.com>
2023-07-01 18:07:52 -0500

    free-agent

---
+++ src/components/features/freeagent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './freeagent.css';
+
+function FreeAgent() {
+  return (
+    <>
+    <a href='https://www.nba.com/players/free-agent-tracker/2023'>
+        <div className='free-agent-box'>
+            <h1 className='free-agent-title'>NBA Free Agent Tracker</h1>
+        </div>
+    </a>
+      
+    </>
+  )
+}
+
+export default FreeAgent;
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.css';
 import Intro from '../features/intro';
 import Stat from '../features/stat';
+import FreeAgent from '../features/freeagent';
 
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <div>
       <Intro />
       <Stat />
+      <FreeAgent />
     </div>
   )
 }
